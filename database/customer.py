21905780d7da1683f90c1d429213299b38c99f12
Hrishikesh Hegishte <hrishikeshhegishte11@gmail.com>
2023-04-17 22:08:42 +0530

    user login code

--- database/customer.py
+++ database/customer.py
@@ -1,5 +1,6 @@
 import pymongo
 connect_string = 'mongodb+srv://Hrishi:Hrishi123@cluster0.fkj1i6o.mongodb.net/test?retryWrites=true&w=majority'
+from bson import json_util
 
 my_client = pymongo.MongoClient(connect_string)
 # First define the database name
@@ -14,7 +15,7 @@ customer = {
 }
 
 # Insert the documents
-collection_name.insert_many([customer])
+#collection_name.insert_many([customer])
 # Check the count
 count = collection_name.count_documents({})
 print(count)
@@ -24,3 +25,12 @@ customer_details = collection_name.find({})
 # Print on the terminal
 for r in customer_details:
     print(r["username"], format("\n"))
+
+def get_customer(uname, pword):
+	customer_details = collection_name.find({"username": uname, "password": pword})
+	if(customer_details == None):
+		return "User not found \n"
+	else:
+		return json_util.dumps(customer_details)
+
+print(get_customer("user", "password"))
