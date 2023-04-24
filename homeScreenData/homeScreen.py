4128bb1efe0899ffd749daa1ae8c319380360669
Hrishikesh Hegishte <hrishikeshhegishte11@gmail.com>
2023-04-24 15:01:33 +0530

    Added function to get top 5 most viewed products

--- homeScreenData/homeScreen.py
+++ homeScreenData/homeScreen.py
@@ -1,13 +1,24 @@
 import pymongo
 connect_string = 'mongodb+srv://Hrishi:Hrishi123@cluster0.fkj1i6o.mongodb.net/test?retryWrites=true&w=majority'
 
+my_client = pymongo.MongoClient(connect_string)
+dbname = my_client['sample_products']
+collection_name = dbname["product_details"]
+
 def getHomeScreenItems():
-    my_client = pymongo.MongoClient(connect_string)
-    dbname = my_client['sample_products']
-    collection_name = dbname["product_details"]
     product_details = collection_name.aggregate([ { "$sample" : { "size": 5 } } ])
     # for i in product_details:
     print(type(product_details))
     return product_details
 
-#print(getHomeScreenItems())
+def getMostViewedItems():
+    product_details = collection_name.aggregate([ { "$sort" : { "view_count": 1 } }, {"$limit": 5} ])
+    # for i in product_details:
+    print(type(product_details))
+    return product_details
+
+#print(getMostViewedItems())
+
+for document in getMostViewedItems():
+    print(document)
+    print("\n")
