908f80bc9aa0174eb3e9fa6c5df723ece50de370
Vedant Otari <vedantmotari24@gmail.com>
2023-05-07 00:19:33 +0530

    Developer page changes.

--- frontend/src/code/Developers.js
+++ frontend/src/code/Developers.js
@@ -9,9 +9,9 @@ export default function Developers() {
   return (
     <>
       <Header showButton="show" />
-      <div className="bg-gray-800 text-center text-white w-5/6 m-auto rounded-2xl mt-10 shadow-2xl shadow-black">
-        <div className="h-60 bg-transparent mb-16 relative">
-          <h1 className="text-6xl mb-4 absolute z-10 text-center w-full h-full flex justify-center items-center font-bold">Developers</h1>
+      <div className="mb-10 bg-gray-800 text-center text-white w-5/6 m-auto rounded-2xl mt-10 shadow-2xl shadow-black">
+        <div className="h-60 bg-transparent relative">
+          <h1 className="text-7xl mb-4 absolute z-10 text-center w-full h-full flex justify-center items-center font-bold">Developers</h1>
           <img
             className="w-full h-full object-cover opacity-40 absolute rounded-t-2xl"
             src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
@@ -19,32 +19,100 @@ export default function Developers() {
           />
 
         </div>
-        <div className="rounded-3xl p-5 py-10 text-white my-4 hover:bg-blue-700 scale-105 duration-500">
+        <div className="rounded-3xl hover:shadow-black hover:shadow-xl p-5 hover:pt-9 text-white my-4 hover:bg-sky-700 hover:scale-105 duration-700">
           <label className="font-serif text-4xl">Hrishikesh Hegishte</label>
           <br />
-          A05 <br />
-          1920000075 <br />
+          <div className="w-5/6 my-6 mx-auto">
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+              <img title="Roll Number" src="./user.png" alt="" className="h-7 align-text-bottom inline pr-2"/>
+              A-05
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="PRN" src="./id_card.png" alt="" className="h-7 align-baseline inline pr-2"/>
+            1920000075
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="LinkedIn" src="./lin.png" alt="" className="h-6 align-baseline inline pr-2"/>
+
+              <a href="https://www.linkedin.com/in/niketan-raskar-663bb7189/" target="_blank">niketan-raskar-663bb7189</a>
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="GitHub" src="./github.png" alt="" className="h-7 align-baseline inline pr-2"/>
+              <a href="https://github.com/hrishi135" target="_blank">hrishi135</a>
+            </div>
+          </div>
         </div>
         <div className="bg-white h-1"></div>
-        <div className="rounded-3xl p-5 py-10 text-white my-4 hover:bg-blue-700 scale-105 duration-500">
+        <div className="rounded-3xl hover:shadow-black hover:shadow-xl p-5 hover:pt-9 text-white my-4 hover:bg-sky-700 hover:scale-105 duration-700">
           <label className="font-serif text-4xl">Vedant Otari</label>
           <br />
-          A20 <br />
-          1920000264 <br />
+          <div className="w-5/6 my-6 mx-auto">
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+              <img title="Roll Number" src="./user.png" alt="" className="h-7 align-text-bottom inline pr-2"/>
+              A-20
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="PRN" src="./id_card.png" alt="" className="h-7 align-baseline inline pr-2"/>
+            1920000264
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="LinkedIn" src="./lin.png" alt="" className="h-6 align-baseline inline pr-2"/>
+
+              <a href="https://www.linkedin.com/in/vedant-otari/" target="_blank">vedant-otari</a>
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="GitHub" src="./github.png" alt="" className="h-7 align-baseline inline pr-2"/>
+              <a href="https://github.com/Vedant-Otari" target="_blank">Vedant-Otari</a>
+            </div>
+          </div>
         </div>
         <div className="bg-white h-1"></div>
-        <div className="rounded-3xl p-5 py-10 text-white my-4 hover:bg-blue-700 scale-105 duration-500">
+        <div className="rounded-3xl hover:shadow-black hover:shadow-xl p-5 hover:pt-9 text-white my-4 hover:bg-sky-700 hover:scale-105 duration-700">
           <label className="font-serif text-4xl">Sanket Khorate</label>
           <br />
-          A30 <br />
-          1920000369 <br />
+          <div className="w-5/6 my-6 mx-auto">
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+              <img title="Roll Number" src="./user.png" alt="" className="h-7 align-text-bottom inline pr-2"/>
+              A-30
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="PRN" src="./id_card.png" alt="" className="h-7 align-baseline inline pr-2"/>
+            1920000369
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="LinkedIn" src="./lin.png" alt="" className="h-6 align-baseline inline pr-2"/>
+
+              <a href="https://www.linkedin.com/in/sanket-khorate/" target="_blank">sanket-khorate</a>
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="GitHub" src="./github.png" alt="" className="h-7 align-baseline inline pr-2"/>
+              <a href="https://github.com/SanketK-25" target="_blank">SanketK-25</a>
+            </div>
+          </div>
         </div>
         <div className="bg-white h-1"></div>
-        <div className="rounded-3xl p-5 py-10 text-white my-4 hover:bg-blue-700 scale-105 duration-500">
+        <div className="rounded-3xl hover:shadow-black hover:shadow-xl p-5 hover:pt-9 text-white my-4 hover:bg-sky-700 hover:scale-105 duration-700">
           <label className="font-serif text-4xl">Niketan Raskar</label>
           <br />
-          A53 <br />
-          1920000660 <br />
+          <div className="w-5/6 my-6 mx-auto">
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+              <img title="Roll Number" src="./user.png" alt="" className="h-7 align-text-bottom inline pr-2"/>
+              A-53
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="PRN" src="./id_card.png" alt="" className="h-7 align-baseline inline pr-2"/>
+            1920000660
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="LinkedIn" src="./lin.png" alt="" className="h-6 align-baseline inline pr-2"/>
+
+              <a href="https://www.linkedin.com/in/hrishikesh-hegishte-0971251b7/" target="_blank">niketan-raskar-663bb7189</a>
+            </div>
+            <div className="w-1/2 py-4 text-left pl-[15%] inline-block text-xl font-sans p-0 m-0">
+            <img title="GitHub" src="./github.png" alt="" className="h-7 align-baseline inline pr-2"/>
+              <a href="https://github.com/Niketan2001" target="_blank">Niketan2001</a>
+            </div>
+          </div>
         </div>
       </div>
     </>
