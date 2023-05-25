b099f45054eed760976b894071de9951e2cd6109
Niketan2001 <72155531+Niketan2001@users.noreply.github.com>
2023-05-25 12:00:02 +0530

    signupchanges

--- frontend/src/code/Signup.js
+++ frontend/src/code/Signup.js
@@ -9,30 +9,74 @@ export default function Signup() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [confirm, setConfirm] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "Email is required" }));
+    } else if (!emailRegex.test(email)) {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "Invalid email format" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+    }
+  };
+
+  const validateUsername = () => {
+    if (!username) {
+      setErrors((prevErrors) => ({ ...prevErrors, username: "Username is required" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, username: "" }));
+    }
+  };
+
+  const validatePassword = () => {
+    if (!password) {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "Password is required" }));
+    } else if (password.length < 8) {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "Password should be at least 8 characters" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "" }));
+    }
+  };
+
+  const validateConfirmPassword = () => {
+    if (!confirm) {
+      setErrors((prevErrors) => ({ ...prevErrors, confirm: "Confirm password is required" }));
+    } else if (password !== confirm) {
+      setErrors((prevErrors) => ({ ...prevErrors, confirm: "Passwords do not match" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, confirm: "" }));
+    }
+  };
 
   const signUp = async () => {
-    console.log(username, email, password);
-    if (password === confirm) {
-      await axios
-        .get("http://127.0.0.1:8000/api/signUp", {
-          params: { username: username, email: email, password: password },
-        })
-        .then((res) => {
-          console.log(res);
-          var data = res.data[0];
+    validateEmail();
+    validateUsername();
+    validatePassword();
+    validateConfirmPassword();
 
-          alert(data.msg);
-          if (data.msg === "Account created succesfully") {
-            console.log("redirect to login");
-            window.location.href = "/login";
-          }
-          return res.data;
-        })
-        .catch((err) => {
-          console.log(err);
+    if (Object.values(errors).every((error) => !error)) {
+      // All fields are valid, proceed with signup
+      console.log(username, email, password);
+
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/api/signUp", {
+          params: { username: username, email: email, password: password },
         });
-    } else {
-      alert("Password dosent match");
+
+        console.log(response.data);
+        const data = response.data[0];
+
+        
+        if (data.msg === "Account created succesfully") {
+          alert(data.msg);
+          console.log("Redirect to login");
+          window.location.href = "/login";
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -44,56 +88,78 @@ export default function Signup() {
         <div className="bg-white px-10 mb-11 py-7 shadow-md rounded-xl border-2 border-gray-200 w-full max-w-md">
           <h1 className="text-3xl font-medium text-center text-blue-950 font-serif">
             Sign Up
-          </h1>
+            </h1>
           <div className="mt-7">
             <label className="text-lg font-medium">Email</label>
             <input
-              className="w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent"
+              className={`w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent ${
+                errors.email ? "border-red-500" : ""
+              }`}
               type="email"
               placeholder="Enter Your email"
-              onInput={(data) => setEmail(data.target.value)}
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              onBlur={validateEmail}
               required
               minLength={5}
               maxLength={50}
             />
+            {errors.email && <p className="text-red-500">{errors.email}</p>}
+
             <label className="text-lg font-medium">User name</label>
             <input
-              className="w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent"
-              type="User"
+              className={`w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent ${
+                errors.username ? "border-red-500" : ""
+              }`}
+              type="text"
               placeholder="Enter Your User name"
-              onInput={(data) => setUsername(data.target.value)}
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              onBlur={validateUsername}
               required
-              minLength={5}
               maxLength={50}
             />
+            {errors.username && <p className="text-red-500">{errors.username}</p>}
+
             <label className="text-lg font-medium">Password</label>
             <div className="flex items-center">
               <input
-                className="w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent"
+                className={`w-full border-2 mb-5 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent ${
+                  errors.password ? "border-red-500" : ""
+                }`}
                 type="password"
                 placeholder="Enter Your password"
-                onInput={(data) => setPassword(data.target.value)}
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                onBlur={validatePassword}
                 required
                 minLength={8}
                 maxLength={20}
               />
               {/* <img src="./a.png" alt="" srcset="" className="h-3" /> */}
             </div>
+            {errors.password && <p className="text-red-500">{errors.password}</p>}
+
             <label className="text-lg font-medium">Confirm Password</label>
             <input
-              className="w-full border-2 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent"
+              className={`w-full border-2 border-gray-200 outline-gray-500 rounded-md p-2 mt-1 bg-transparent ${
+                errors.confirm ? "border-red-500" : ""
+              }`}
               type="password"
               placeholder="Re-Enter Your password"
-              onInput={(data) => setConfirm(data.target.value)}
+              value={confirm}
+              onChange={(event) => setConfirm(event.target.value)}
+              onBlur={validateConfirmPassword}
               required
               minLength={8}
               maxLength={20}
             />
+            {errors.confirm && <p className="text-red-500">{errors.confirm}</p>}
 
             <div className="mt-8 flex flex-col gap-y-4">
               <button
                 onClick={signUp}
-                className=" active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-950 text-white bg-green-600 text-lg font-bold"
+                className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-950 text-white bg-green-600 text-lg font-bold"
               >
                 Sign up
               </button>
@@ -102,9 +168,7 @@ export default function Signup() {
             <div className="mt-8 flex justify-center items-center">
               <p className="font-medium text-base">Already have an account?</p>
               <Link to="/login">
-                <button className="text-blue-400 font-medium ml-2">
-                  Login
-                </button>
+                <button className="text-blue-400 font-medium ml-2">Login</button>
               </Link>
             </div>
           </div>
@@ -113,3 +177,5 @@ export default function Signup() {
     </>
   );
 }
+
+         
